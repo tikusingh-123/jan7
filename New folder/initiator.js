c6a4b8890c2e2b88612495ea8c4e7f8b51343bd4
tikusingh-123 <75766866+tikusingh-123@users.noreply.github.com>
2021-01-07 18:55:11 +0530

    Add files via upload

---
+++ New folder/initiator.js
@@ -0,0 +1,33 @@
+$("document").ready(function () {
+    $('#PersonTableContainer').jtable({
+        title: 'Table of people',
+        actions: {
+            listAction: '/GettingStarted/PersonList',
+            createAction: '/GettingStarted/CreatePerson',
+            updateAction: '/GettingStarted/UpdatePerson',
+            deleteAction: '/GettingStarted/DeletePerson'
+        },
+        fields: {
+            PersonId: {
+                key: true,
+                list: false
+            },
+            Name: {
+                title: 'Author Name',
+                width: '40%'
+            },
+            Age: {
+                title: 'Age',
+                width: '20%'
+            },
+            RecordDate: {
+                title: 'Record date',
+                width: '30%',
+                type: 'date',
+                create: false,
+                edit: false
+            }
+        }
+    });
+});
+
---
+++ New folder/table.js
@@ -0,0 +1,13 @@
+$("document").ready(function() {
+    $('#example').DataTable( {
+        "ajax": "db.json",
+        "columns": [
+            { "data": "name" },
+            { "data": "position" },
+            { "data": "office" },
+            { "data": "extn" },
+            { "data": "start_date" },
+            { "data": "salary" }
+        ]
+    } );
+} );
